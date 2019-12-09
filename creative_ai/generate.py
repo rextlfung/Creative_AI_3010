4b6edc34b602650f7b0ae8c1b6b54b4141bce83e
Connor <cblesy@umich.edu>
2019-12-08 22:23:59 -0500

    Fixed typo

--- creative_ai/generate.py
+++ creative_ai/generate.py
@@ -13,7 +13,7 @@ from creative_ai.data.dataLoader import *
 from creative_ai.models.musicInfo import *
 from creative_ai.models.languageModel import LanguageModel
 from creative_ai.plotly.barChart import *
-from creative_at.reddit.writePosts import *
+from creative_ai.reddit.writePosts import *
 
 # FIXME Add your team name
 TEAM = 'Garden Man'
